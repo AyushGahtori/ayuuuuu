86263ebe67a3ce4ce974a363d24315ac5da00917
Ayush Gahtori <152148622+AyushGahtori@users.noreply.github.com>
2023-12-25 15:31:28 +0530

    commit

--- ayuuuuu/src/App.tsx
+++ ayuuuuu/src/App.tsx
@@ -23,13 +23,11 @@ const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
-        {/* public routes */}
-        <Route element={<AuthLayout />}>
-          <Route path="/sign-in" element={<SigninForm />} />
-          <Route path="/sign-up" element={<SignupForm />} />
-        </Route>
+        
+        
+    
 
-        {/* private routes */}
+        
         <Route element={<RootLayout />}>
           <Route index element={<Home />} /> 
           <Route path="/explore" element={<Explore />} />
